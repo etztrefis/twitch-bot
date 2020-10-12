fc6e7c2096b2f64073114e54f5bd6403776e7f5a
etztrefis <etztrefishello@gmail.com>
2020-10-12 10:42:41 +0700

    ping command refactored into async

--- commands/ping/index.js
+++ commands/ping/index.js
@@ -1,9 +1,9 @@
 module.exports = {
     Name: "ping",
     Author: "trefis",
+    Aliases: ["pang","peng","pong","pung","pyng", "ping"],
+    Date: "10/12/2020, 10:12:26 AM",
     Code: (async function ping() {
-        return {
-            replay: `Pong! Reporting for duty! Okayeg ...`.toString()
-        };
+        return `Pong! Reporting for duty! Okayeg ...`;
     })
 };

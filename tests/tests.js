27d22d082722e5e0cc9083611d4442cb75289a4b
etztrefis <etztrefishello@gmail.com>
2020-10-16 09:48:49 +0700

    some tests idk

--- tests/tests.js
+++ tests/tests.js
@@ -2,24 +2,21 @@ repos(`etztrefis`, `--full`).then((messages) => {
     console.log(messages)
 });
 
-async function repos(user, showFullData) {
-    const axios = require('axios').default;
-    const { data } = await axios({
-        method: 'get',
-        url: `https://api.github.com/users/${user}/repos`,
-        responseType: 'json',
-    });
+    async function repos(user, showFullData) {
+        const axios = require('axios').default;
+        const os = require('os');
+        const { data } = await axios({
+            method: 'get',
+            url: `https://api.github.com/users/${user}/repos`,
+            responseType: 'json',
+        });
+        const messages = showFullData === `--full` ? 
+        (data || []).map(function(repoData){
+            return ` N: ${repoData.name}, F:  ${repoData.fork}, L:  ${repoData.language} ❗ `;
+        }) : (data || []).map(function(repoData){
+            return ` N: ${repoData.name}❗ `;
+        });
 
-    const messageHandler = showFullData === `--full` ? githubReposFullName : githubReposName;
-    const messages = (data || []).map(messageHandler);
+        return messages.join(os.arch);
     // data = > [1,2,3] messageHandler = func(arg) { return arg + 1;} => messages.map(messageHandler) = > messages = [2,3,4]
-    return messages;
-}
-
-function githubReposName(repoData) {
-    return ` N: ${repoData.name}❗ `;
-}
-
-function githubReposFullName(repoData) {
-    return ` N: ${repoData.name}, F:  ${repoData.fork}, L:  ${repoData.language} ❗ `;
-}
+    }

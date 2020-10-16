0ac74418a295aee67b076fa0937653e20e76f248
etztrefis <etztrefishello@gmail.com>
2020-10-16 10:07:16 +0700

    some final changes

--- commands/repositories/index.js
+++ commands/repositories/index.js
@@ -2,7 +2,7 @@ module.exports = {
     Name: "repos",
     Author: "trefis",
     Aliases: ["reps", "repos"],
-    Date: "", //LATER
+    Date: "10/16/2020, 10:04:56 AM", 
     Code: (async function repos(user, showFullData) {
         const axios = require('axios').default;
         const os = require('os');
@@ -11,8 +11,6 @@ module.exports = {
             url: `https://api.github.com/users/${user}/repos`,
             responseType: 'json',
         });
-        //TypeError: Cannot read property 'name' of undefined : because of this.FullReposName() \ this.ReposName() need to refactor messsageHandler or 
-        //refuse to using array.map(), im tired
         const messages = showFullData === `--full` ? 
         (data || []).map(function(repoData){
             return ` N: ${repoData.name}, F:  ${repoData.fork}, L:  ${repoData.language} ❗ `;
@@ -20,6 +18,6 @@ module.exports = {
             return ` N: ${repoData.name}❗ `;
         });
 
-        return messages.join(os.arch());
+        return messages.join(os.arch);
     }),
 };

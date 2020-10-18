6636c63029777b3c2f53399d9d26df7711864790
etztrefis <etztrefishello@gmail.com>
2020-10-18 23:27:16 +0700

    python command added 4HEad

---
+++ commands/python/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    Name: "python",
+    Author: "trefis",
+    Aliases: ["python"],
+    Date: "10/17/2020, 1:14:00 PM",
+    Code: (async function python() {
+        return `https://clips.twitch.tv/ExpensiveWonderfulClamArsonNoSexy`;
+    })
+};

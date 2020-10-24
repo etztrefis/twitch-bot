ff152ca0dfa5852e0ab9a6c1dcb62efb1aa4bca6
etztrefis <etztrefishello@gmail.com>
2020-10-24 19:19:06 +0700

    dotenv for connet to db

--- options.js
+++ options.js
@@ -4,6 +4,11 @@ const options = {
     username: process.env.USERNAME,
     password: process.env.PASSWORD,
     prefix: "`",
+    user: process.env.USER,
+    dbname: process.env.DBNAME,
+    pass: process.env.PASS,
+    host: process.env.HOST,
+    dialect: "mysql"
 };
 
 module.exports = options;

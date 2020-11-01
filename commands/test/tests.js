7dcdf5798aea5f5dc2e4b9966f64d945e4aab88b
etztrefis <etztrefishello@gmail.com>
2020-11-01 17:01:27 +0700

    some new tests

--- commands/test/tests.js
+++ commands/test/tests.js
@@ -1,14 +1,52 @@
-const { Sequelize } = require("sequelize");
-const { QueryTypes }
-const options = require("../options.js")
+// const { Sequelize } = require("sequelize");
+// const { QueryTypes }
+// const options = require("../options.js")
 
-const sequelize = new Sequelize(options.dbname, options.user, options.pass, {
-    host: options.host,
-    dialect: options.dialect
-})
+// const sequelize = new Sequelize(options.dbname, options.user, options.pass, {
+//     host: options.host,
+//     dialect: options.dialect
+// })
 
-async function borobushe_loh() {
-    let arg = "ping";
-    const query = await sequelize.query('SELECT * FROM Commands WHERE Name = ')
+// async function borobushe_loh() {
+//     let arg = "ping";
+//     const query = await sequelize.query('SELECT * FROM Commands WHERE Name = ')
 
+// }
+
+// var axios = require('axios');
+
+// var config = {
+//     method: 'get',
+//     url: 'https://api.twitch.tv/helix/users?login=trefis&=',
+//     headers: {
+//         'Client-Id': 'ept4kn251t3kwqawgrd9u9q4uexlva',
+//         'Authorization': 'Bearer 65fbnv57chah7ww17nx82bh4lt7nh4'
+//     }
+// };
+
+// axios(config)
+//     .then(function (response) {
+//         console.log(response.status)
+//         console.log(response.data.data);
+//         let uid = JSON.stringify(response.data.data[0]['id']);
+//         console.log(uid)
+//     })
+//     .catch(function (error) {
+//         console.log(error);
+//     });
+
+const axios = require('axios').default;
+allo("trefis");
+async function allo(user) {
+    require("dotenv").config({ path: "../../.env" });
+    const { data } = await axios({
+        method: 'get',
+        url: 'https://api.twitch.tv/helix/users?login=gjaskgjnaskdjgasd',
+        responseType: 'json',
+        headers: {
+            'Client-Id': process.env.KEYVALUE, //ept4kn251t3kwqawgrd9u9q4uexlva
+            'Authorization': process.env.BEARER //Bearer 65fbnv57chah7ww17nx82bh4lt7nh4
+        }
+    });
+    console.log(data.data[0]['id'])
 }

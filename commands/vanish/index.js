dadfe32601a658dcff3d27a8bc0a6a6ac9bc661d
etztrefis <etztrefishello@gmail.com>
2020-11-02 20:05:22 +0700

    vanish command added

---
+++ commands/vanish/index.js
@@ -0,0 +1,21 @@
+module.exports = {
+    Name: "vanish",
+    Author: "trefis",
+    Aliases: ["vanish", "vanishme"],
+    Date: "11/2/2020, 7:56:08 PM",
+    Code: (async function vanish(context) {
+        if (context.badges.hasModerator) {
+            return `I cannot time moderators out! monkaS`
+        } else if (context.badges.hasBroadcaster) {
+            return `Why are you trying to vanish in your own channel? 4Head`
+        } else if (context.badges.hasStaff) {
+            return "I cannot time Twitch staff out! monkaS"
+        } else if (context.badges.hasGlobalMod) {
+            return { reply: "I cannot time global moderators out! monkaS" };
+        } else if (context.badges.hasAdmin) {
+            return { reply: "I cannot time Twitch administrators out! monkaS" };
+        } else {
+            return `/timeout ${context.displayName} 1 vanished`
+        }
+    })
+}

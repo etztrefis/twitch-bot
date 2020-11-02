060e298e4317a4ae4afdc1bd9c21fc0768890f56
etztrefis <etztrefishello@gmail.com>
2020-11-02 20:27:36 +0700

    some testing FeelOkayMan

--- commands/test/tests.js
+++ commands/test/tests.js
@@ -1,17 +1,17 @@
-// const { Sequelize } = require("sequelize");
-// const { QueryTypes }
-// const options = require("../options.js")
+const { Sequelize } = require("sequelize");
+const { QueryTypes }
+const options = require("../options.js")
 
-// const sequelize = new Sequelize(options.dbname, options.user, options.pass, {
-//     host: options.host,
-//     dialect: options.dialect
-// })
+const sequelize = new Sequelize(options.dbname, options.user, options.pass, {
+    host: options.host,
+    dialect: options.dialect
+})
 
-// async function borobushe_loh() {
-//     let arg = "ping";
-//     const query = await sequelize.query('SELECT * FROM Commands WHERE Name = ')
+async function borobushe_loh() {
+    let arg = "ping";
+    const query = await sequelize.query('SELECT * FROM Commands WHERE Name = ')
 
-// }
+}
 
 // var axios = require('axios');
 
@@ -35,18 +35,18 @@
 //         console.log(error);
 //     });
 
-const axios = require('axios').default;
-allo("trefis");
-async function allo(user) {
-    require("dotenv").config({ path: "../../.env" });
-    const { data } = await axios({
-        method: 'get',
-        url: 'https://api.twitch.tv/helix/users?login=gjaskgjnaskdjgasd',
-        responseType: 'json',
-        headers: {
-            'Client-Id': process.env.KEYVALUE, //ept4kn251t3kwqawgrd9u9q4uexlva
-            'Authorization': process.env.BEARER //Bearer 65fbnv57chah7ww17nx82bh4lt7nh4
-        }
-    });
-    console.log(data.data[0]['id'])
-}
+// const axios = require('axios').default;
+// allo("trefis");
+// async function allo(user) {
+//     require("dotenv").config({ path: "../../.env" });
+//     const { data } = await axios({
+//         method: 'get',
+//         url: 'https://api.twitch.tv/helix/users?login=gjaskgjnaskdjgasd',
+//         responseType: 'json',
+//         headers: {
+//             'Client-Id': process.env.KEYVALUE, //ept4kn251t3kwqawgrd9u9q4uexlva
+//             'Authorization': process.env.BEARER //Bearer 65fbnv57chah7ww17nx82bh4lt7nh4
+//         }
+//     });
+//     console.log(data.data[0]['id'])
+// }

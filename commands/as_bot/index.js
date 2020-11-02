8096c5fde7d36eb03daffcb6d048378ece749deb
etztrefis <etztrefishello@gmail.com>
2020-11-02 20:04:54 +0700

    as_bot command added

---
+++ commands/as_bot/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    Name: "as_bot",
+    Author: "trefis",
+    Aliases: ["as_okayeg", "okayeg"],
+    Date: "",
+    Code: (async function as_bot(context) {
+        return context;
+    })
+}

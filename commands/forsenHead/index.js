d8af5a550e6ce36dbb6b5ccda3dac249d5c4e38a
etztrefis <etztrefishello@gmail.com>
2020-11-06 18:48:18 +0700

    forsenHead command was added

---
+++ commands/forsenHead/index.js
@@ -0,0 +1,20 @@
+module.exports = {
+    Name: "forsenHead",
+    Author: "trefis",
+    Aliases: ["yourmom", "forsenhead", "mom"],
+    Date: "11/6/2020, 6:45:40 PM",
+    Description: "Posts random joke about your mom.",
+    Code: (async function forsenHead() {
+        const axios = require("axios").default;
+        const { data } = await axios({
+            method: 'get',
+            url: `https://trefis.net/mom_jokes.json`,
+            responseType: 'json',
+        });
+
+        const values = Object.values(data.data);
+        const random = Math.floor(Math.random() * values.length);
+        const radomValue = values[random];
+        return `${radomValue} forsenHead`;
+    })
+};

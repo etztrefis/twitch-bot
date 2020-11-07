478e5848102d70b33c13ba23ca14a5333c0dff74
etztrefis <etztrefishello@gmail.com>
2020-11-07 14:05:57 +0700

    uptime into the ping command

--- commands/ping/index.js
+++ commands/ping/index.js
@@ -5,6 +5,20 @@ module.exports = {
     Date: "10/12/2020, 10:12:26 AM",
     Description: "Ping!",
     Code: (async function ping() {
-        return `Pong! Reporting for duty! Okayeg PETTHEPEEPO `;
+        const process = require('process');
+
+        let ut_sec = process.uptime();
+        let ut_min = ut_sec / 60;
+        let ut_hour = ut_min / 60;
+
+        ut_sec = Math.floor(ut_sec);
+        ut_min = Math.floor(ut_min);
+        ut_hour = Math.floor(ut_hour);
+
+        ut_hour = ut_hour % 60;
+        ut_min = ut_min % 60;
+        ut_sec = ut_sec % 60;
+
+        return `Pong! Reporting for duty! Okayeg PETTHEPEEPO  Uptime: ${ut_hour}h, ${ut_min}m, ${ut_sec}s;`;
     })
 };

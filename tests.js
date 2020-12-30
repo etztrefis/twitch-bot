cafc8fdf40766360133b6600e0049ed0a780260b
etztrefis <etztrefishello@gmail.com>
2020-12-30 14:26:16 +0700

    removed useless logging queries for cooldowns

--- tests.js
+++ tests.js
@@ -1,29 +1,29 @@
-const { Sequelize } = require("sequelize");
-const { QueryTypes } = require("sequelize");
-require("dotenv").config({ path: "./.env" });
+// const { Sequelize } = require("sequelize");
+// const { QueryTypes } = require("sequelize");
+// require("dotenv").config({ path: "./.env" });
 
-borobushe_loh();
-async function borobushe_loh() {
-	const sequelize = new Sequelize(
-		process.env.DBNAME,
-		process.env.USER,
-		process.env.PASS,
-		{
-			host: process.env.HOST,
-			dialect: "mysql",
-		}
-	);
-	const query = await sequelize.query("SELECT Name FROM Commands", {
-		type: QueryTypes.SELECT,
-	});
-	const response = [];
-	if (query.length > 0) {
-		query.forEach((value, index) => {
-			response.push(query[index].Name);
-		});
-	}
-	console.log(response.join(", "));
-}
+// borobushe_loh();
+// async function borobushe_loh() {
+// 	const sequelize = new Sequelize(
+// 		process.env.DBNAME,
+// 		process.env.USER,
+// 		process.env.PASS,
+// 		{
+// 			host: process.env.HOST,
+// 			dialect: "mysql",
+// 		}
+// 	);
+// 	const query = await sequelize.query("SELECT Name FROM Commands", {
+// 		type: QueryTypes.SELECT,
+// 	});
+// 	const response = [];
+// 	if (query.length > 0) {
+// 		query.forEach((value, index) => {
+// 			response.push(query[index].Name);
+// 		});
+// 	}
+// 	console.log(response.join(", "));
+// }
 
 // const fs = require("fs");
 // fs.readFile("./data/mom_jokes.json", (err, data) => {
@@ -40,5 +40,5 @@ async function borobushe_loh() {
 //     method: 'get',
 //     url: `https://trefis.net/mom_jokes.json`,
 //     responseType: 'json',
-// }).then(responseconsole.log(data));
+// }).then(responseconsole	log(data));
 // axios.get('https://trefis.net/mom_jokes.json').then(response => console.log(response.data));
